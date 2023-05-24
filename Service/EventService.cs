dcf880e70b7a2e68c5ea662638c747977a37b3c3
Vladimir Glisic <vglisic@seatgeek.com>
2023-05-24 10:56:19 +0200

    logger added

--- Service/EventService.cs
+++ Service/EventService.cs
@@ -5,8 +5,17 @@ namespace Nugget2.Service
 {
     public class EventService : IEventService
     {
+        private readonly ILogger<EventService> logger;
+
+        public EventService(ILogger<EventService> logger)
+        {
+            this.logger = logger;
+        }
+
         public IEnumerable<Event> Get()
         {
+            logger.LogDebug("Get method is called.");
+
             return new[] {
                 new Event { Name = "Event A", Date = new DateTime(2023, 6, 1, 20, 0, 0)},
                 new Event { Name = "Event B", Date = new DateTime(2023, 6, 2, 20, 0, 0)},
